74ac3c3ded443759b56b704ae691eb758a585f7a
braojntu <braojntu@gmail.com>
2021-10-14 13:22:42 +0530

    added mongo cloud url

--- createMongoConn.js
+++ createMongoConn.js
@@ -2,12 +2,12 @@ import { MongoClient } from 'mongodb';
 import bcrypt from 'bcrypt';
 
 export async function createConnection() {
-    const MONGO_URL = "mongodb://localhost/users";
-    //const MONGO_URL = process.env.MONGO_URL;
+    //const MONGO_URL = "mongodb://localhost/users";
+    const MONGO_URL = process.env.MONGO_URL;
     const client = new MongoClient(MONGO_URL);
     await client.connect();
-    console.log("Successfully Connected to local MongoDB");
-    //console.log("Successfully Connected to cloud MongoDB");
+    //console.log("Successfully Connected to local MongoDB");
+    console.log("Successfully Connected to cloud MongoDB");
     return client;
     // console.log(users);
 }
